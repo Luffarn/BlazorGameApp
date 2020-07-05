3adbdc1dd0edfed6b219cbbeb4bd0881c859b4fa
SimonBonnier <simon.bonnier.developer@gmail.com>
2020-07-05 10:04:08 +0200

    Add collision detection for ball object

--- BreakoutGameObjects/Base/GameObject.cs
+++ BreakoutGameObjects/Base/GameObject.cs
@@ -11,7 +11,7 @@ namespace BreakOutGame.BreakoutGameObjects.Base
     {
         protected int GameWidth;
         protected int GameHeight;
-        protected Position Position { get; set; }
+        public Position Position { get; set; }
         protected Speed Speed { get; set; } = new Speed(0, 0);
         public abstract Task Draw(Canvas2DContext context);
         public abstract void Update();
