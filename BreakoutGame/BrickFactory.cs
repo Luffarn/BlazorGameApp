431f73d0c535c994a012fe9a71501622c1f3510a
SimonBonnier <simon.bonnier.developer@gmail.com>
2020-07-10 21:27:24 +0200

    Add concept of levels

---
+++ BreakoutGame/BrickFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace BreakOutGame.BreakoutGame
+{
+    using BreakOutGame.BreakoutGame.GameObjects;
+    using System.Collections.Generic;
+
+    public class BrickFactory
+    {
+        public static List<Brick> CreateBricks(int[][] brickLayout, int gameWidth, int gameHeight)
+        {
+            var bricks = new List<Brick>();
+            for (int i = 0; i < brickLayout.Length; i++)
+            {
+                for (int j = 0; j < brickLayout[i].Length; j++)
+                {
+                    if (brickLayout[i][j] == 1)
+                    {
+                        int xPos = (j * (brickLayout.Length + 77)) + 5;
+                        int yPos = i * (brickLayout[i].Length + 30) + 5;
+                        bricks.Add(new Brick(xPos, yPos));
+                    }
+                }
+            }
+            return bricks;
+        }
+    }
+}
